5eec813708cccdfab721652faf569a63f7681a5b
Martin Fritz <martinfri@edu.aau.at>
2022-03-13 00:33:30 +0100

    Test von Eingabefeld, Buttons und  Ausgabefelder

--- app/src/main/java/com/example/se2einzelphasefritz/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphasefritz/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.se2einzelphasefritz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText mtn = findViewById(R.id.editTextNumberMtn);
+
+        Button btnTestServer = findViewById(R.id.btnTestServer);
+        Button btnCalculateQuersumme = findViewById(R.id.btnSumCheck);
+
+        TextView showReplySRV = findViewById(R.id.textViewReplyServer);
+        TextView showResultQuersumme = findViewById(R.id.textViewQuersumme);
+
+        btnTestServer.setOnClickListener(view -> {
+           showReplySRV.setText("Test");
+        });
+
+        btnCalculateQuersumme.setOnClickListener(view -> {
+            String mtnNumber = mtn.getText().toString();
+            int test = Integer.parseInt(mtnNumber);
+            test = test + 1;
+            mtnNumber = String.valueOf(test);
+            showResultQuersumme.setText(mtnNumber);
+        });
+
     }
 }
